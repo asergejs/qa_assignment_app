e8afcaa6e5a7d53877860dfdb8a27e71dfe289a1
Sergejs Arapovs <sergejs.arapovs@wandoofinance.com>
2023-08-21 10:51:16 +0300

    json converter

--- src/main/java/com/wandoofinance/qahomework/mapper/PaymentEntityMapper.java
+++ src/main/java/com/wandoofinance/qahomework/mapper/PaymentEntityMapper.java
@@ -1,18 +1,24 @@
 package com.wandoofinance.qahomework.mapper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wandoofinance.qahomework.domain.dto.RegistrationRequestDTO;
 import com.wandoofinance.qahomework.domain.dto.TransactionRequestDTO;
 import com.wandoofinance.qahomework.domain.entity.Payment;
 import com.wandoofinance.qahomework.domain.entity.User;
 import com.wandoofinance.qahomework.domain.model.TransactionType;
+import lombok.SneakyThrows;
+
+import static com.wandoofinance.qahomework.JsonConverter.toJsonString;
 
 public class PaymentEntityMapper {
 
+    @SneakyThrows
     public static Payment transactionReqToPaymentEntity(TransactionRequestDTO transactionRequestDTO, TransactionType type, User user) {
         var paymentEntity = new Payment();
         paymentEntity.setType(type);
         paymentEntity.setAmount(transactionRequestDTO.getAmount().getAmount());
         paymentEntity.setUser(user);
+        paymentEntity.setRawResponse(toJsonString(transactionRequestDTO));
         return paymentEntity;
     }
 
